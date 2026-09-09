57a20c805f4ba998595a48b6c0b69baef54784d7
Igor Burian <proqramista@gmail.com>
2026-09-09 16:58:16 +0200

    dodanie podstawowego layoutu, ekranu dodaj aktywność

--- app/src/main/java/com/example/ekran_startowy/DodawanieAktywnosci.kt
+++ app/src/main/java/com/example/ekran_startowy/DodawanieAktywnosci.kt
@@ -1,6 +1,5 @@
 package com.example.ekran_startowy
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -8,22 +7,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class DodawanieAktywnosci : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_dodawanie_aktywnosci)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        //DODANIE FUNKCJONALNOŚĆI PRZYCISKU "DODAJ AKTYWNOŚĆ"
-        val button = findViewById<Button>(R.id.dodaj_aktywnosc)
-        button.setOnClickListener {
-            val intent = Intent(this, DodawanieAktywnosci::class.java)
-            startActivity(intent)
         }
     }
 }
